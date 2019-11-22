5bb57917b9449f3ac6e8ded887398dd56500bbbc
byFluerash <fluerashdaniil@gmail.com>
2019-11-22 21:56:52 +0400

    пробный залив Добавил в Balance методы получения: конкретных рассходов и доходов; отдельно все доходы; отдельно все расходы. Добавил class Controller описал несколько методов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,15 @@
 package com.company;
-import java.util.ArrayList;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        CategoryOfCosts category = new CategoryOfCosts("fun");
 
-
-	Balance balance = new Balance();
-	Cost cost = new Cost(10000,category);
-	balance.addCost(cost);
-	balance.addRevenue(new Revenue(15000,new CategoryOfRevenue("grant")));
-	System.out.println(balance.getBalance());
+     System.out.println("Проверить Баланс");
+     System.out.println("Добавить расход/доход");
+     System.out.println("Удалить расход/доход");
+     System.out.println("Проверить расход/доход");
+     boolean menu = false;
 
     }
 }
